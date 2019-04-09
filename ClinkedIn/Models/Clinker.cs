e3fa50f088a17429de7f59d9cf303bd1c7b196f5
Jasmine Walters <jsmnwltrs@gmail.com>
2019-04-08 20:15:10 -0500

    setup clinker class

---
+++ ClinkedIn/Models/Clinker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinkedIn.Models
+{
+    public class Clinker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public int PrisonId { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public List<int> Interests { get; set; }
+        public List<int> Services { get; set; }
+        public List<int> Friends { get; set; }
+        public List<int> Enemies { get; set; }
+
+        public Clinker(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+}
