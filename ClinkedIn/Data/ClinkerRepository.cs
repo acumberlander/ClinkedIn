73f7a811e378cd48da558c50d6a81d18a45e1270
Marshall Offutt <marshalloffutt@gmail.com>
2019-04-08 21:23:02 -0500

    Adds register clinker controller, clinker request model and validator

---
+++ ClinkedIn/Data/ClinkerRepository.cs
@@ -0,0 +1,24 @@
+﻿using ClinkedIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Data
+{
+    public class ClinkerRepository
+    {
+        static List<Clinker> _clinkers = new List<Clinker>();
+
+        public Clinker AddClinker(string name, string password)
+        {
+            var newClinker = new Clinker(name, password);
+
+            newClinker.Id = _clinkers.Count + 1;
+
+            _clinkers.Add(newClinker);
+
+            return newClinker;
+        }
+    }
+}
---
+++ ClinkedIn/Models/CreateClinkerRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Models
+{
+    public class CreateClinkerRequestValidator
+    {
+        public bool Validate(CreateClinkerRequest requestToValidate)
+        {
+            return string.IsNullOrEmpty(requestToValidate.Name)
+                || string.IsNullOrEmpty(requestToValidate.Password);
+        }
+    }
+}
