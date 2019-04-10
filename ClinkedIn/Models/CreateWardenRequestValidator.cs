263d7aa460f944998bb47d39df715ba342c4f531
unknown <marshalloffutt@gmail.com>
2019-04-10 18:51:19 -0500

    adds register new warden controller

---
+++ ClinkedIn/Models/CreateWardenRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Models
+{
+    public class CreateWardenRequestValidator
+    {
+        public bool Validate(CreateWardenRequest requestToValidate)
+        {
+            return string.IsNullOrEmpty(requestToValidate.Name)
+                || string.IsNullOrEmpty(requestToValidate.Password);
+        }
+    }
+}
