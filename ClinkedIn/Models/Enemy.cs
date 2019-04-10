f2a919e4f2c21f44d0a8824e034182107dc61679
Jasmine Walters <jsmnwltrs@gmail.com>
2019-04-09 20:31:36 -0500

    added enemy class, repository, and controller

---
+++ ClinkedIn/Models/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Models
+{
+    public class Enemy
+    {
+        public int ClinkerOneId { get; set; }
+        public int ClinkerTwoId { get; set; }
+        public int Id { get; set; }
+
+    }
+}
