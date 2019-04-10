18394206f574ec5bf7f9bdfcc28b9083ad5d55a5
Jasmine Walters <jsmnwltrs@gmail.com>
2019-04-09 20:11:28 -0500

    WIP

--- ClinkedIn/Models/Clinker.cs
+++ ClinkedIn/Models/Clinker.cs
@@ -12,10 +12,10 @@ namespace ClinkedIn.Models
         public int Age { get; set; }
         public int PrisonId { get; set; }
         public DateTime ReleaseDate { get; set; }
-        public List<int> Interests { get; set; }
+        public List<Interests> Interests { get; set; }
         public List<int> Services { get; set; }
-        public List<int> Friends { get; set; }
-        public List<int> Enemies { get; set; }
+        public List<Clinker> Friends { get; set; }
+        public List<Clinker> Enemies { get; set; }
 
         public Clinker(string name, string password)
         {
