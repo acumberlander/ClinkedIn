41aafd8f5795bbfa1468a1c962d46ad4226beaf6
unknown <marshalloffutt@gmail.com>
2019-04-10 18:08:31 -0500

    creates warden class

---
+++ ClinkedIn/Models/Warden.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinkedIn.Models
+{
+    public class Warden
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public Warden(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+}
