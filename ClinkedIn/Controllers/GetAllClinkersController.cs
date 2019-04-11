9d0edb22b2e0c911741e78eba4ffb7d66a869bc5
unknown <marshalloffutt@gmail.com>
2019-04-10 19:09:31 -0500

    wip

---
+++ ClinkedIn/Controllers/GetAllClinkersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedIn.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinkedIn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllClinkersController : ControllerBase
+    {
+        readonly ClinkerRepository _clinkerRepository;
+
+        public GetAllClinkersController()
+        {
+            _clinkerRepository = new ClinkerRepository();
+        }
+
+        [HttpGet("allclinkers")]
+        public ActionResult GetAllClinkers()
+        {
+            var clinkerList = _clinkerRepository;
+
+            return Created($"api/getAllClinkers", clinkerList);
+        }
+    }
+}
