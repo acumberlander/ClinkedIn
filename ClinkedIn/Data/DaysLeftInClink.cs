ad258915328c6246e39aff3828c8ddcc44b59a94
Austin <austincumberlander@gmail.com>
2019-04-13 12:28:47 -0500

    days left in clink feature is working. going to clean up the date format of what returns

---
+++ ClinkedIn/Data/DaysLeftInClink.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinkedIn.Models;
+
+namespace ClinkedIn.Data
+{
+    public class DaysLeftInClink
+    {
+        public double GetDaysLeftInClink(DateTime releaseDate)
+        {
+            //releaseDate = clinker.ReleaseDate;
+            DateTime today = DateTime.Now;
+            double daysLeftInClink = (releaseDate - today).TotalDays;
+
+            return daysLeftInClink;
+        }
+    }
+}
